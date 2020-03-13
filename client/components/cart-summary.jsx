54924c558c144880c87840214ae6bf6c4b5cf39e
stevemarshall-89 <stevenmarshall1989@gmail.com>
2020-03-13 16:13:05 -0700

    Edited crass empty cart text

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
-  if (!props) {
-    return <div><h1>Cart Is Empty, Buy Some Shit!</h1></div>;
+  if (!props.cartItems.length) {
+    return (
+      <div className="cart-page container">
+        <div className="cart-header mb-3">
+          <div className="mb-4">
+            <a onClick={() => props.view('catalog', {})}><i className="fas fa-chevron-left"></i> Back To Catalog</a>
+          </div>
+          <h1 className="mb-3">My Cart</h1>
+        </div>
+        <div className="m-5 p-5"><h1 className="text-muted text-center">--Cart is Currently Empty--</h1></div>
+        <div className="cart-footer mb-4">
+          <h1 className="text-right mr-3">Subtotal: $0.00</h1>
+          <h3 className="text-right mr-3">Estimated Tax: $0.00</h3>
+          <h3 className="text-right mr-3">Shipping: FREE</h3>
+        </div>
+      </div>
+    );
   }
   const cartList = Object.values(props.cartItems);
   const cartListArray = cartList.map(cart => {
